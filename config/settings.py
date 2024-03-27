f7a06d39593ed38b1c0089d586e63f0d4790ee98
HamedMirzaeiOfficial <HamedMirzaei2001Official@gmail.com>
2024-03-27 15:54:49 +0330

    adding admin panel and something else

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'employee', 
 ]
 
 MIDDLEWARE = [
@@ -123,6 +124,7 @@ STATICFILES_DIRS = [
     'config/static',
 ]
 
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
